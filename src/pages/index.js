bdf572b0c7c8e2d1263265e04caf9d3245ecc1c7
jiaxi9427 <Peng.jia@northeastern.edu>
2024-02-12 19:09:56 -0500

    2/12 update2

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,17 @@
 import React from 'react';
 import styles from './qcomps/firstcomp'
 import Profile from './qcomps/profile_mistake'
+import Props from './qcomps/props_item';
+import List from './qcomps/list_keys_id';
+import Recipe from './qcomps/recipes';
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <Props />
+        <List />
+        <Recipe />
     </div>
   )
 }
